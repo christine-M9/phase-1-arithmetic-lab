ee9c03ca4cc8ee9a82db9d2f626d08a2a2001747
Christine <christinekhekama99@gmail.com>
2023-06-15 15:22:23 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+console.log(random);
+
+let num3 = 20;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+let numbers = [6, 20, 12, 9, 18];
+let max = Math.max(...numbers);
+
+console.log(max); // Output: 20
